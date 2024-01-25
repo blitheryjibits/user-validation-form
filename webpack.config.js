c9fbc62a38314e7ab6dcccade6f6e734494336d4
robert thornton <robert.thornton92@gmail.com>
2024-01-25 15:07:13 +0200

    Created - config file to handle website packaging. Contains html, partialHtml, file, and css loaders.

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPartialsPlugin = require('html-webpack-partials-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+
+module.exports = {
+    entry: {
+        main: './src/index.js',
+    },
+    
+    mode: process.env.NODE_ENV === "production" ? "production" : "development",
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template.html'
+        }),
+        new HtmlWebpackPartialsPlugin({
+            path: './src/countryList.html',
+            template: 'template.html',
+            location: 'replace-me',
+            priority: 'replace'
+                
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+            chunkFilename: '[id].css'
+        })
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(js)$/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                use: [
+                  {
+                    loader: 'url-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: '',
+                        publicPath: './',
+                        url: true,
+                        import: true
+                    }
+                  }
+                ]
+              }
+        ],
+    },
+};
+
