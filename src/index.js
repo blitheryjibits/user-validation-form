ca9da988b21cf62c531493efc8b5fc161784c70d
robert thornton <robert.thornton92@gmail.com>
2024-01-25 15:49:35 +0200

    Added - background image for the html body displaying the form. Added the image in index.js as a css style property. Changed css file to place form in the center of body element and removed background-image property.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-// import img from './background.jpg';
 const gatherDOMElements = require('./formValidation').gatherDOMElements;
 const validate = require('./formValidation').validate;
 const [form, emailInput, passwordInput, passwordConfirmation, country, zipcode] = gatherDOMElements();
 
+// add background to html body
+const body = document.querySelector('body');
+body.setAttribute(
+    "style",
+    "background-image: url('./background.jpg')"
+);
 
-
+// initialize submit button
 form.addEventListener('submit', validate);
